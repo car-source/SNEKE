cb21c33dbfa0e0e13fd865dd310a6ca31bf97eb5
andracar000 <andracar000@warren.k12.in.us>
2025-11-07 12:24:07 -0500

    fixed day 01

--- day01/snake.py
+++ day01/snake.py
@@ -6,14 +6,32 @@ pygame.init()
 # Set display window
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+display_surface = pygame.display.set_mode(size)
 pygame.display.set_caption("~~SNEKE~~")
 
 # Set FSP and clock
+# TODO: make a variable (constant) called FPS and initialize to 20
+clock = pygame.time.Clock()
 
 # Set game values
+# TODO: make a variable (constant) named SNAKE_SIZE and initialize to 20
+
+# TODO: make a variable named head_x and assign half of the window_width to it. use integer divison // (i.e. 11 / 2 is 5.5, 11//2 is 5)
+# TODO: make a variable named head_y and assign half of the window_height + 100 to it. use integer divison //
+
+# TODO: make a variable named snake_dx and assign 0 to it
+# TODO: repeat for a variable named snake_dy
+
+# TODO: repeat for a variable named score and assign 0 to it.
 
 # Set colors
+GREEN = (0, 255, 0) # (r, g, b)
+DARKGREEN = (10, 50, 10)
+RED = (255, 0, 0)
+DARKRED = (150, 0, 0)
+WHITE = (255, 255, 255)
+
 
 # Set fonts
 
@@ -35,7 +53,7 @@ while running:
         # Move the snake
 
     # Add the head coordinate to the first index of the body coordinate list
-    # This will essentilalyl move all of the snakes body by one position in the list
+    # This will essentially move all the snakes body by one position in the list
 
     # Update the x,y position of the snakes head and make a new coordinate
 
